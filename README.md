71b58c9e7fd17979911e9bac5e38f5d21638b1a8
nmaltsev <nsmalcev@gmail.com>
2024-12-20 10:45:03 +0100

    add http proxy

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # docker-sample
 A basic project to generate a docker image
+
+## Endpoints
+- http proxy: http://localhost:8009/proxy/raw.githubusercontent.com/nmaltsev/fm/refs/heads/main/src/favicon.svg
--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from aiohttp import web
+from aiohttp import web, ClientSession
 import os
 import logging
 
@@ -72,12 +72,49 @@ async def test3_handler(request):
     response.content_type = "text/html"
     return response
 
+async def proxy(request, url:str):
+    async with ClientSession(auto_decompress=False) as session:
+        async with await session.request(request.method, url) as proxy_request:
+            if proxy_request.status not in {201, 202, 200}:
+                return None
+            log.debug("Prequest h: %s %s", proxy_request.headers, proxy_request)
+            print(proxy_request)
+            print(request)
+            proxy_response = web.StreamResponse(
+                status=proxy_request.status,
+                reason=proxy_request.reason,
+            )
+            proxy_response.headers.update(proxy_request.headers)
+            await proxy_response.prepare(request)
+            while True:
+                chunk = await proxy_request.content.read()
+                if not chunk:
+                    break
+                await proxy_response.write(chunk)
+            await proxy_response.write_eof()
+            return proxy_response
+
+async def proxy_handler(request):
+    host = request.match_info.get("host")
+    path = request.match_info.get("path")
+    log.debug("Host %s, Path %s", host, path)
+    print(f"{host=}, {path=}")
+    response = await proxy(request, "https://" + host + "/" + path)
+    if response is not None:
+        return response
+    return web.FileResponse(
+        headers={"Cache-Control": "max-age=6000, immutable"},
+        path=(os.path.dirname(__file__) or ".") + "/image.svg",
+    )
+    
+
 app = web.Application()
 app.add_routes([
     web.get("/", index_handler),
     web.get("/page1", test1_handler),
     web.get("/page2", test2_handler),
     web.get("/page3", test3_handler),
+    web.get("/proxy/{host:[^\\/]+}/{path:.*}", proxy_handler)
 ])
 
 
